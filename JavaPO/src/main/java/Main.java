d174e4bfbb1347c57e96dfec39f7710fcf8eafda
Byczax <byczqq136@gmail.com>
2020-04-22 19:47:36 +0200

    działająca podstawa
    Signed-off-by: Byczax <byczqq136@gmail.com>

--- JavaPO/src/main/java/Main.java
+++ JavaPO/src/main/java/Main.java
@@ -11,65 +11,78 @@ public class Main {
         Random rand = new Random();
         int n = rand.nextInt(100);
         System.out.println("\n"+n+" - Losowa liczba");
-        int[][] tablica = new int [100][100];
+        int populacja = 10;
+        int[][] tablica = new int [populacja][populacja];
         for (int i = 0; i < tablica.length; i++) {
             for (int j = 0; j < tablica[i].length; j++) {
                 tablica[i][j] = 0;
             }
         }
-        int X = rand.nextInt(100);
-        int Y = rand.nextInt(100);
+        int X = rand.nextInt(populacja-2)+1;
+        int Y = rand.nextInt(populacja-2)+1;
         System.out.println("\n"+X+" - Losowa liczba");
         System.out.println("\n"+Y+" - Losowa liczba");
-        tablica[X+1][Y+1]=1;
+        tablica[X][Y]=1;
         int ilosc = 1;
 
-        while (ilosc>0) {
-            for(int i=1;i<99;i++){
-                for(int j=1;j<99;j++){
+        while (ilosc<(populacja-2)*(populacja-2)) {
+            for(int i=1;i<populacja-1;i++){
+                for(int j=1;j<populacja-1;j++){
                     if(tablica[i][j]==1){
                             if (tablica[i-1][j]==0) {
                                 if (rand.nextInt(2)==1) {
                                     tablica[i-1][j]=1;
-                                    ilosc++;
+                                    if(i-1!=0)
+                                    {
+                                        ilosc++;
+                                    }
                                 }
                             }
 
                             if (tablica[i+1][j]==0) {
                                 if (rand.nextInt(2)==1) {
                                     tablica[i+1][j]=1;
-                                    ilosc++;
+                                    if(i+1!=populacja-1)
+                                    {
+                                        ilosc++;
+                                    }
                                     }
                                 }
 
                             if (tablica[i][j-1]==0) {
                                 if (rand.nextInt(2)==1) {
                                     tablica[i][j-1]=1;
-                                    ilosc++;
+                                    if(j-1!=0)
+                                    {
+                                        ilosc++;
+                                    }
                                 }
                             }
 
                             if (tablica[i][j+1]==0) {
                                 if (rand.nextInt(2)==1) {
                                     tablica[i][j+1]=1;
-                                    ilosc++;
+                                    if(j+1!=populacja-1)
+                                    {
+                                        ilosc++;
+                                    }
                                 }
                             }
                     }
                 }        
             }
-            for(int i=0;i<100;i++){
-                for(int j=0;j<100;i++){
+            for(int i=1;i<populacja-1;i++){
+                for(int j=1;j<populacja-1;j++){
                     if(tablica[i][j]==1)
                     {
-                        System.out.print("x ");
+                        System.out.print("# ");
                     }
                     else
-                        System.out.print("o ");
+                        System.out.print("O ");
                 }
                 System.out.print("\n");
             }
+            System.out.print("\n\n\n");
         }
-        System.out.flush();
     }
 }
